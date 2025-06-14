e08bfdaf6b6c362922ded664f5825b69fd64fdb9
Benson Ma <bensonma415@meta.com>
2025-06-13 22:29:59 -0700

    wip

--- fbgemm_gpu/experimental/gen_ai/test/quantize/quantize_test.py
+++ fbgemm_gpu/experimental/gen_ai/test/quantize/quantize_test.py
@@ -41,7 +41,9 @@ except ImportError:
 
 running_on_github: bool = os.getenv("GITHUB_ENV") is not None
 
-if torch.cuda.is_available() and supports_float8_fnuz():
+if torch.cuda.is_available() and supports_float8_fnuz(
+    throw_on_hip_incompatibility=(not running_on_github)
+):
     # Supported FP8 format is different on NV and AMD.
     fp8_e4m3: torch.dtype = torch.float8_e4m3fnuz
     fp8_e5m2: torch.dtype = torch.float8_e5m2fnuz
